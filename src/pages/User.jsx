86ad66db4926897752db994e536460587546ea9b
sstokes <sterlingjstokes@gmail.com>
2025-01-29 18:23:11 -0500

    Setup fetch request for the user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,13 +1,44 @@
 import react from 'react'; 
 import Profile from '../components/Common/Profile'
 import NavBar from '../components/Common/NavBar';
+import Table from '../components/Common/Table';
 
+
+import {useState, useEffect} from 'react';
 export default function UserPage() {
+
+    const [profileData, setData] = useState('null');
+
+    const getData = async () => {
+        try {
+            const [userData, accountData] = await Promise.all([
+                fetch('http://localhost:8080/api/users').then(data => data.json()),
+                fetch(`http://localhost:8080/api/accounts`).then(data => data.json()),
+            ])
+            setData({userData, accountData});
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    // loaded function for when data is fetched.
+  const loaded = () => {
     return (
-        <>
-            <NavBar/>
-            <Profile/>
-        </>
-        
+      <>
+        <pre>{JSON.stringify(profileData,null,2)}</pre>
+      </>
     );
+  };
+
+  // Function for when data doesn't exist.
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+    return profileData ? loaded() : loading();
 }
+
