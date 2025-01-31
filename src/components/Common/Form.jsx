ecd6460866e5f855edaeda635ab003b93ab6184a
sstokes <sterlingjstokes@gmail.com>
2025-01-31 14:44:22 -0500

    Added a form

--- src/components/Common/Form.jsx
+++ src/components/Common/Form.jsx
@@ -5,55 +5,62 @@ import { calcVolumePoint } from 'three/examples/jsm/curves/NURBSUtils.js';
 
 export default function Form ({type, title}){
 
-    const [inputValue, setInputValue] = useState(null);
-    const c = undefined;
+    // let c = undefined;
+    // const getData = async (a) => {
 
-    const getData = async (a) => {
-
-        const b = await fetch('http://192.168.1.162:8080/api/accounts').then(data => data.json());
-        //Object deconstrunct
-        c = b;
-    }
-
-
-    if(type == "update-account" | "update-transaction"){
-        useEffect(() => {
-            getData(type);
-            console.log("Update form data is mounted");
-        },[])
-    }
+    //     const b = await fetch('http://192.168.1.162:8080/api/accounts').then(data => data.json());
+    //     //Object deconstrunct
+    //     c = b;
+    // }
+    // if(type == "update-account" | "update-transaction"){
+    //     useEffect(() => {
+    //         getData(type);
+    //         //configure state pass
+    //         console.log("Update form data is mounted");
+    //     },[])
+    // }
     
-
-
-
-
-
+    const [inputValue, setInputValue] = useState({accountType: '',inputType: ''});
     const handleChange = (event) => {
-
-
+        const {name,value} = event.target;
+        setInputValue({
+            ...inputValue, 
+            [name]: value});
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('FormSubmitted')
+        console.log('Form Submitted')
     }
     const redirect = (event) => {
         event.preventDefault();
+        console.log("Redirect back to Dashboard");
         console.log('Form not Submitted redirect');
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <h1>{title}</h1>
-            {validateFormType(type, inputValue,handleChange, c)};
+            {/* {validateFormType(type, inputValue,handleChange, c)} */}
+            <label>Account Type
+            <select name="accountType" value={inputValue.accountType} onChange={handleChange}>
+                <option value="Saving">Saving</option>
+                <option value="Checking">Checking</option>
+            </select>
+            </label>
+            <label>Current Amount
+            <input type="number" name="inputType" value={inputValue.inputType} 
+            placeholder={0.00} onChange={handleChange}/>
+            </label>
             <button type="submit">Submit</button>
             <button type="submit" onClick={redirect}>Cancel</button> {/*Redirect to the previous page*/}
         </form>
     );
 }
 
-function validateFormType(type,inputValue, handleChange, otherdata = undefined) {
+function validateFormType(type,inputValue, handleChange) {
     switch (type) {
         case 'create-account':
+
             return createAccount(inputValue, handleChange);
         case 'update-account':
             return updateAccount();
@@ -64,17 +71,18 @@ function validateFormType(type,inputValue, handleChange, otherdata = undefined) 
     }
 }
 
-
 function createAccount(inputValue, handleChange) {
     return(
         <>
-            <label>Account Type</label>
+            <label>Account Type
             <select name="accountType" onChange={handleChange}>
                 <option value="Saving">Saving</option>
                 <option value="Checking">Checking</option>
             </select>
-            <label>Current Amount</label>
+            </label>
+            <label>Current Amount
             <input type="number" min ="1" step="any" placeholder={0.00} onChange={handleChange}/>
+            </label>
         </>
     );
 }
