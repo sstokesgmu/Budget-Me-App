124aff29afcc428a028a6485d72fc507b6c657f0
sstokes <sterlingjstokes@gmail.com>
2025-02-04 22:21:34 -0500

    working on handling data from the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,94 @@
-import react from 'react'
+import react,{useEffect,useState}from 'react'
 import NavBar from '../components/Common/NavBar'
 import TransactionChart from '../components/Chart/TransChart';
+import Profile from '../components/Common/Profile';
 import ModalForm from '../components/Modals/ModalForm';
 
 export default function Dashboard(){
+    //States we need to manage is the change in bucket and the change in accounts
+    const [selectedAccount, setSelectedAccount] = useState(null);
+    const [selectedBucket,setSelectedBucket] = useState({}); 
+
+    
+    const [accounts, setAccounts] = useState([]);
+    const [buckets, setBuckets] = useState(null);
+    const [data, setData] = useState(null); 
+
+
+    const mount = async() => {
+        try{    
+            const user = await fetch('https://budgetapp-vdsp.onrender.com/api/users?fields=accounts')
+                                    .then(data => data.json());
+            const accounts = user[0].accounts;
+            const buckets = await fetch(`https://budgetapp-vdsp.onrender.com/api/transactions/${accounts[0]}`).then(data => data.json());
+            //we'regetting the first of the return
+        
+            //save all possiblities to state
+            setAccounts(accounts);
+            setBuckets(buckets);
+
+            setSelectedAccount(accounts[0]);
+            setSelectedBucket(bucket[0]._id);
+
+        } catch(e){
+            console.error("Error fetching data");
+        }
+    }
+    useEffect(()=>{
+        mount();
+        console.log('The page is mounted');
+    },[]);
+
+
+
+
+
+    useEffect(()=>{
+        //Get 
+    },[selectedAccount]);
+    // useEffect(()=>{
+
+    // },[selectedBucket])
+
+
+
+    // useEffect(() => {})
+
+        console.log(buckets)
+        console.log(accounts);
 
 
     //Data we need accounts (number, bucket) transaction buckets of those account 
     return( 
     <>
-        {/* <pre>{JSON.stringify(profileData,null,2)}</pre> */}
+            <pre>{JSON.stringify(selectedAccount, null, 2)}</pre> {/* JSON stringify to display object */}
+            <pre>{JSON.stringify(selectedBucket, null, 2)}</pre> {/* Display bucket id if it's an object */}
+            <div>
+                <h3>Accounts Menu</h3>
+                <select>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                </select>
+            </div>
+
+            
+            <div>
+                <h3>BucketMenu</h3>
+                <select>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                </select>
+            </div>
+
+
+        {/* <pre>{JSON.stringify(profileData,null,2)}</pre>
         <NavBar/>
+        //Todo: Preformatted Text
         <div style={{display:'flex'}}>
             <section style={{width:'20%',border:"solid"}}>
-                {/* <Card renderStyle={"card-profile-small"} data={{src:"src/assets/sfa3-ken2.jpg", alt:"Ken Picture"}}/> */}
+                <Profile renderStyle={'profile_small'} data={{src:"src/assets/sfa3-akuma2.jpg", alt:"Akuma Picture"}}/>
                 <section style={{backgroundColor:'red', height:'30%', position:'relative', top:'6rem'}}>
                     hello
                 </section>
@@ -36,7 +111,7 @@ export default function Dashboard(){
                 <p>Timeframe Between 1/29 to 1/102</p>
             </section>
         </div>    
-        <ModalForm/>
+        <ModalForm/> */}
     </>
     );
 }
